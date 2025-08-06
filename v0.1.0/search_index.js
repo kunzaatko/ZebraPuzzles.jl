c50f41f7c4f328b13579197d5fe60766a39bbfda
Documenter.jl <documenter@juliadocs.github.io>
2025-08-06 13:18:15 +0000

    build based on ca5d0db

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ZebraPuzzles","page":"Home","title":"ZebraPuzzles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ZebraPuzzles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
