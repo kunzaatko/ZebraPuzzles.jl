224c5d65150e84de79387db1f820e781e89c6fd1
Martin Kunz <martinkunz@email.cz>
2025-08-21 11:47:47 +0200

    fix(test): Avoid duplicate clues in test for MinimalityViolation

--- test/puzzles.jl
+++ test/puzzles.jl
@@ -6,4 +6,10 @@ z = deepcopy(ZP.EINSTEINS_ZEBRA)
 empty!(z.clues)
 @test all(ZP.check(z, c) for c in ZP.EINSTEINS_ZEBRA.clues)
 @test ZP.add_clues!(z, ZP.EINSTEINS_ZEBRA.clues) isa ZebraPuzzle
-@test_throws ZP.MinimalityViolation ZP.add_clue!(z)
+@test_throws ZP.MinimalityViolation begin
+    c = rand(Clue, ZP.EINSTEINS_ZEBRA)
+    while !(ZP.check_duplicate(ZP.EINSTEINS_ZEBRA, c; throw_error=false))
+        c = rand(Clue, ZP.EINSTEINS_ZEBRA)
+    end
+    ZP.add_clue!(z, c)
+end
